675414f5fb1865b837b5f2bb66663264c19bccc3
Abdul <awsayed@alaska.edu>
2019-06-02 05:07:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,34 @@
 # Helper Method
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+  !(board[index].nil? || board[index] == " " || board[index] == "")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.find { |win_combination|
+    win_combination.all? { |win_index|
+      board[win_index] == "X"
+    } || win_combination.all? { |win_index|
+      board[win_index] == "O"
+    }
+  }
+end
+
+def full?(board)
+    board.all? { |elem|
+        elem == "X" || elem == "O"
+    }
+end
+  
+
